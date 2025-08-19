79a9f7d3d0c7f7b20d2ef5c342efd2a6de4fdd94
Alex Yee <yeealexi@gmail.com>
2025-08-19 14:55:48 -0700

    add level 3 spawnpoints

--- scripts/global.gd
+++ scripts/global.gd
@@ -3,5 +3,5 @@ extends Node
 # Constants
 var rng := RandomNumberGenerator.new()
 @export var DEBUG_MODE := true
-@export var LEVEL_COUNTDOWN_TIME := 3.0 if not DEBUG_MODE else 3.0
+@export var LEVEL_COUNTDOWN_TIME := 3.0 if not DEBUG_MODE else .2
 # Utility Functions

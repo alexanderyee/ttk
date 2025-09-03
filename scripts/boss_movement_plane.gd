a87c1f316692e7c9f04e86c462c64d4e742c1a39
Alex Yee <yeealexi@gmail.com>
2025-09-02 17:29:44 -0700

    fix easing on boss movement

--- scripts/boss_movement_plane.gd
+++ scripts/boss_movement_plane.gd
@@ -24,26 +24,26 @@ func _process(_delta: float) -> void:
 				DebugDraw3D.draw_line(top_right_global, bottom_right_global, Color.FUCHSIA)
 				DebugDraw3D.draw_line(bottom_right_global, bottom_left_global, Color.FUCHSIA)
 				DebugDraw3D.draw_line(bottom_left_global, top_left_global, Color.FUCHSIA)
-				var cell_center: Vector3 = get_cell_center_global_transform(c, r)
+				var cell_center: Vector3 = get_cell_center_global_position(c, r)
 				DebugDraw3D.draw_sphere(cell_center, .2, Color.AQUA)
 			
 	pass
 
-func get_cell_center_global_transform(col: int, row: int) -> Vector3:
+func get_cell_center_global_position(col: int, row: int) -> Vector3:
 	if row >= rows or col >= cols:
 		push_warning("Requested cell is out of bounds: " + str(col) + ", " + str(row))
 		return Vector3.ZERO
-	var local_x := float(col) / float(cols)	* size_x + cell_width / 2.0
-	var local_y := float(row) / float(rows)	* size_y + cell_height / 2.0
+	var local_x := float(col) / float(cols) * size_x + cell_width / 2.0
+	var local_y := float(row) / float(rows) * size_y + cell_height / 2.0
 	var local := Vector3(0.0, local_y - size_y / 2.0, local_x - size_x / 2.0)
 	
 	return to_global(local)
 
 func get_random_cell(current: Vector2i = Vector2i(-1, -1)) -> Vector2i:
 	var random_cell := current
-	while (random_cell - current).length() > 1:
-		random_cell = Vector2i(Global.rng.randi_range(0, cols), Global.rng.randi_range(0, rows))
+	while (random_cell - current).length() <= 2:
+		random_cell = Vector2i(Global.rng.randi_range(0, cols - 1), Global.rng.randi_range(0, rows - 1))
 	return random_cell
 	
 func get_center_cell() -> Vector2i:
-	return Vector2i(cols / 2, rows / 2)
+	return Vector2i(floor(cols / 2.0), floor(rows / 2.0))
--- scripts/left_hand_boss_enemy.gd
+++ scripts/left_hand_boss_enemy.gd
@@ -1,18 +1,21 @@
 class_name LeftHandBossEnemy extends Enemy
 
 var movement_plane: BossMovementPlane
-@onready var pause_movement_timer: Timer = $PauseMovementTimer
 var current_cell: Vector2i
 var cell_to_move_to: Vector2i 
+var moving_time := 0.0
+var total_move_time := 2.5
+@onready var pause_movement_timer: Timer = $PauseMovementTimer
+
 
 func _ready():
 	
 	super._ready()
 
 func _process(delta: float) -> void:
-	if pause_movement_timer.is_stopped():
+	if pause_movement_timer.is_stopped() and cell_to_move_to == current_cell:
 		pause_movement_timer.start(Global.rng.randf_range(1.0, 2.0))
-	if cell_to_move_to:
+	if cell_to_move_to != current_cell:
 		move_to_cell(delta)
 	super._process(delta)
 	
@@ -45,10 +48,21 @@ func play_hit_animation():
 
 
 func _on_pause_movement_timer_timeout() -> void:
+	pause_movement_timer.stop()
 	# find a new cell on the boss movement plane and move to it
 	if not current_cell:
 		current_cell = movement_plane.get_center_cell()
 	cell_to_move_to = movement_plane.get_random_cell(current_cell)
+	moving_time = 0.0
 	
 func move_to_cell(delta: float) -> void:
-	return
+	moving_time += delta
+	var current_cell_pos = movement_plane.get_cell_center_global_position(current_cell.x, current_cell.y)
+	var next_cell_pos = movement_plane.get_cell_center_global_position(cell_to_move_to.x, cell_to_move_to.y)
+	var u = clamp(moving_time / total_move_time, 0.0, 1.0)
+	u = u * u * (3.0 - 2.0 * u)
+	
+	global_position = current_cell_pos.lerp(next_cell_pos, u)
+	if u >= 1.0:
+		current_cell = cell_to_move_to
+		
